2befe0d0775e048bd4b513ff0e50109fe265594c
Jung-woo Lee <jwl@Jung-wooui-MacBook.local>
2019-07-12 15:23:20 +0900

    acc2

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class accountInfo(models.Model):
+    university=models.CharField(max_length=200,null=True)
+    department=models.CharField(max_length=200,null=True)
+    name=models.CharField(max_length=200,null=True)
+    nickname=models.CharField(max_length=200,null=True)
+    introduction=models.TextField(null=True)
+    Fin = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib import auth
+from .models import accountInfo
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+            auth.login(request, user)
+            Info=accountInfo()
+            Info.university=request.POST['university']
+            Info.department=request.POST['department']
+            Info.name=request.POST['name']
+            Info.nickname=request.POST['nickname']
+            Info.introduction=request.POST['introduction']
+            Info.save()
+           
+            return redirect('home')
+    return render(request, 'signup.html')
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
+    return render(request, 'signup.html')
