fdac07c2af34411d8e77c536fcd864f8b71a7525
leejunghyun <cxz9080@gmail.com>
2019-07-12 13:44:45 +0900

    mypage앱 profile 틀 구현

---
+++ mypage/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.profile, name="profile"),
+    path('commissioned/', views.commissioned, name="commissioned"),
+    path('performing/', views.performing, name="performing"),
+    path('scrap/', views.scrap, name="scrap"),
+
+]
--- mypage/views.py
+++ mypage/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def commissioned(request):
+    return render(request, 'commissioned.html')
+
+def performing(request):
+    return render(request, 'performing.html')
+
+def scrap(request):
+    return render(request, 'scrap.html')
