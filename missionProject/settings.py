69ba37b7d44390374a944970de9fe9e84b4edf9e
leejunghyun <cxz9080@gmail.com>
2019-07-18 16:37:54 +0900

    이메일 인증 test 성공

--- missionProject/settings.py
+++ missionProject/settings.py
@@ -124,3 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = 'cxz9080@gmail.com'
+EMAIL_HOST_PASSWORD = 'Coke9080'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
