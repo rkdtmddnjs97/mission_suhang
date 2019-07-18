6e2968f2eb1a492b73e21db2596590f25f6fac76
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2019-07-18 19:17:34 +0900

    미션 수행하는 거 crud 개잘만듬 ㅇㅈ ? ㅇㅇㅈ

--- missionProject/settings.py
+++ missionProject/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- missionProject/urls.py
+++ missionProject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 import main.views
 import view_request.urls
 import view_request.views
+import request.urls
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -27,6 +28,7 @@ urlpatterns = [
     path('', main.views.home, name="home"),
     path('mypage/', include('mypage.urls')),
     path('accounts/', include('accounts.urls')),
-    path('crud/',include(view_request.urls)),
+    path('request/', include('request.urls')),
+    # path('crud/',include(view_request.urls)),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
    
