132cec08d2ecab07a4270df1e3e34fa4f07ad209
leejunghyun <cxz9080@gmail.com>
2019-07-22 16:21:30 +0900

    이미 존재하는 해시태그는 해당 데이터를 가져와서 연결되게 함. 지려부렷다.

--- request/admin.py
+++ request/admin.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
-from .models import Post,Comment
+from .models import Post,Comment,Hashtag
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
-
+admin.site.register(Hashtag)
