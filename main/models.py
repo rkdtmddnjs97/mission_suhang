463345d7b823742af0c816a5b411023faab1f8fd
Jung-woo Lee <jwl@Jung-wooui-MacBook.local>
2019-07-30 21:19:07 +0900

    공지사항

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Announcement(models.Model):
+    title=models.CharField(max_length=200,null=True)
+    writer=models.CharField(max_length=200, null=True)
+    content=models.TextField(null=True)
+    announcement_img=models.ImageField(upload_to='announcement_img/', null=True, blank=True)
+    announcement_file=models.FileField(upload_to='announcement_file/', null=True, blank=True)
+    pub_date = models.DateTimeField('Date published',null=True)
 # Create your models here.
