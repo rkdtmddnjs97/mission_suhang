d5a063d48667ea367aaee0b5630e33140b47b71f
leejunghyun <cxz9080@gmail.com>
2019-07-30 18:52:16 +0900

    알림센터에 쪽지(chat) 알림기능 추가

--- main/context_processors.py
+++ main/context_processors.py
@@ -9,4 +9,4 @@ def add_variable_to_context(request):
         user = Profile.objects.get(profile_id=request.user.username)
         notifications = Notification.objects.filter(to=user)
         notifi_count = notifications.count()
-        return {'notifications': notifications, 'notifi_count':notifi_count}
+        return {'notification_list': notifications, 'notifi_count':notifi_count}
