eff5f0c836ec46c8b769b144f5c06f39356e99b4
rkdtmddnjs97 <rkdtmddnjs97@gmail.com>
2019-08-10 03:11:53 +0900

    ㅈㅅ 다시 바꿈

--- missionProject/settings.py
+++ missionProject/settings.py
@@ -21,9 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '7isg#bcw9lv!azbwf@d$i@wby@o+hm4aw4#7j%(o1_7o(4cqhh'
-
+# SECRET_KEY= os.environ.get('DJANGO_SECRET_KEY', '7isg#bcw9lv!azbwf@d$i@wby@o+hm4aw4#7j%(o1_7o(4cqhh')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+#DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+
 
 ALLOWED_HOSTS = ["*"]
 
